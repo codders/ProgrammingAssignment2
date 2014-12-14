5c450b399612d77c1615609e87893508aa5c2b66
Arthur Taylor <arthur@advancedtelematic.com>
2014-12-14 12:52:34 +0100

    Added cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a Matrix with a "cached", or lazily
+## evaluated solution. For some reason this is written as two
+## functions at module scope, instead of encapsulating one
+## inside the other. I suspect the lecturers for this course
+## are not software engineers.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates the so-called 'CacheMatrix'.
+## It returns a list of functions that allow you to get
+## and set the value of the matrix, and to violate the 
+## encapsulation of the "caching" by directly manipulating
+## the value stored in the cache.
+makeCacheMatrix <- function(storedMatrix = matrix()) {
+  cachedInverse <- NULL
+  set <- function(newMatrix) {
+    storedMatrix <<- newMatrix
+    cachedInverse <<- NULL
+  }
+  get <- function() storedMatrix
+  ## This setInverse function should really be internal to makeCacheMatrix
+  setInverse <- function(inverse) cachedInverse <<- inverse
+  ## This getInverse function should be where the inverse is lazily calculated
+  getInverse <- function() cachedInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function takes the result of a makeCacheMatrix and calculates
+## the inverse of the matrix (unless it has already been calculated).
+## This really ought to be defined inside the lexical scope of makeCacheMatrix,
+## but it isn't because simpletons.
+cacheSolve <- function(cacheMatrix, ...) {
+  inverse <- cacheMatrix$getInverse()
+  if (!is.null(inverse)) {
+    message("Getting cached inverse")
+    return(inverse)
+  }
+  storedMatrix <- cacheMatrix$get()
+  inverse <- solve(storedMatrix)
+  cacheMatrix$setInverse(inverse)
+  inverse
 }
+
+## This is how that function should have looked
+betterCacheMatrix <- function(storedMatrix = matrix()) {
+  cachedInverse <- NULL
+  set <- function(newMatrix) {
+    storedMatrix <<- newMatrix
+    cachedInverse <<- NULL
+  }
+  get <- function() storedMatrix
+  getInverse <- function() {
+    if (is.null(cachedInverse)) {
+      cachedInverse <<- solve(storedMatrix)
+    } else {
+      message("Returning cached inverse")
+    }
+    return(cachedInverse)
+  } 
+  list(set = set, get = get,
+       getInverse = getInverse)
+}
+
